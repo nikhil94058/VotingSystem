1836d2d1a8c29e7fde9dcb55d850a24d1676bda0
GovindSinghh <143206234+GovindSinghh@users.noreply.github.com>
2023-12-04 18:03:26 +0530

    Add files via upload

---
+++ Login/src/main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import image from './image.jpg'
+export default function main(){
+  return(
+    <div className="main">
+        <input type="text" placeholder="Your Name" />
+        <input type="text" placeholder="Your Name" />
+        <button>LOGIN</button>
+      <img src={image} alt="Voting"  className="image"/>
+    </div>
+
+  )
+}
